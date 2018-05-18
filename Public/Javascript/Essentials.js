afed1f292e6585a373293d154bce36204c934c8a
sliwati11 <39227406+sliwati11@users.noreply.github.com>
2018-05-18 03:32:59 +0200

    Die index.html ist clientseitig fast fertig

--- Public/Javascript/Essentials.js
+++ Public/Javascript/Essentials.js
@@ -12,64 +12,7 @@ function rangePicker(){
 }
 
 
-function datePicker() {
-    $(document).ready(function() {
 
-        var start = moment().subtract(29, 'days');
-        var end = moment();
-
-        function cb(start, end) {
-            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-        }
-
-        $('#reportrange').daterangepicker({
-            startDate: start,
-            endDate: end,
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            }
-        }, cb);
-
-        cb(start, end);
-
-    });
-    $(document).ready(function() {
-        $('#datetimepicker1')
-            .datepicker({
-                autoclose: true,
-                format: "dd MM yyyy ",
-                todayHighlight: true,
-                endDate: '12/30/2020',
-                todayBtn: true,
-                pickerPosition: "bottom-left",
-                language: 'pt-BR'
-            })
-
-    });
-    $(document).ready(function() {
-        $('#dateRangePicker2')
-            .datepicker({
-
-                todayHighlight: true,
-                format: "dd MM yyyy ",
-                dateFormat: "yy-mm-dd",
-                timeFormat:  "hh:mm:ss",
-                endDate: '12:30:2020',
-                autoclose: true,
-                todayBtn: true,
-                pickerPosition: "bottom-left",
-                language: 'pt-BR'
-            })
-
-    });
-
-
-}
 function myFunction() {
     var x = window.parent.document.getElementsByName("date")[0];
 
@@ -126,4 +69,4 @@ var FormValidator = (function(){
             tmp.onkeydown = tmp.onkeyup = runChecks;
         }
     }
-})()
+})();
