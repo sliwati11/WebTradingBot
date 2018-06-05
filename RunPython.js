bfa98492872981e03c950ffdebc343a9885bc1c4
sliwati11 <39227406+sliwati11@users.noreply.github.com>
2018-06-05 15:35:11 +0200

    Bei nicht validierten daten auf dem Server wird asynchron <Falsche Eingabe> ausgegeben

--- RunPython.js
+++ RunPython.js
@@ -3,11 +3,11 @@ var PythonShell = require('python-shell');
 
 const PythonModul ={
     init : function () {
-        PythonShell.run('my_script.py', function (err) {
+        PythonShell.run('./GeneticTrader/TradingBot.py', function (err) {
             if (err) throw err;
             console.log('finished');
         });
-    }
+    },
 
 };
 
--- app.js
+++ app.js
@@ -1,15 +1,42 @@
+'use strict'
+
 var express = require('express');
 var app = express();
-var path = require('path');
+var bodyParser = require('body-parser');
+var pythonShell = require('./RunPython');
+var validator = require('../Public/Javascript/Validator');
 
 app.use( '/', express.static('public'));
+//app.use(express.json());
 
+var path = require('path');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/html/index.html'));
+    res.sendFile(path.join(__dirname + '/Public/html/index.html'));
+    //console.log(JSON.stringify(exchange))
+    console.log('Exchange: Localhost');
 
+});
+app.post('/data', function(req, res) {
+    var exchange= req.body;
+    console.log('In Data' );
+    if( validator.validateAgentenAnzahl(exchange.agentenAnzahl) ){
+        if( validator.validateGenerationenAnzahl(exchange.generationAnzahl)){
+            if  (validator.validateEmail(exchange.email_input)){
+                pythonShell.init();
+                res.sendFile(path.join(__dirname + '/Public/html/index.html'));
+                console.log('Start Bot!!!');
+            }
+        }
+    }
+    else {
+        console.log('Cannot start Bot');
+        res.sendFile(path.join(__dirname + '/Public/html/index.html'));
+    }
 
-})
-
+});
 app.listen(80);
+console.log('Server started! At http://localhost:80' );
